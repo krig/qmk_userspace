34dd19943056cacb457573bf04aa02f62666e324
Kristoffer Grönlund <k@ziran.se>
2023-12-27 22:57:11 +0100

    avoid upper pinky

--- users/krig/layout/krigite.h
+++ users/krig/layout/krigite.h
@@ -3,19 +3,19 @@
 //
 // slight mod of graphite
 // by rdavison
-//   b l d w z  ' y o u q
+//   q l d w b  ' y o u -
 //   n r t s g  p h a e i
-//   x j m c v  k f . - ,
+//   x j m c v  k f . z ,
 
 #ifndef ALTERN_L1
 
-#define ALTERN_L1 KC_B, KC_L, KC_D, KC_W, KC_Z
+#define ALTERN_L1 KC_Q, KC_L, KC_D, KC_W, KC_B
 #define ALTERN_L2 KC_N, KC_R, KC_T, KC_S, KC_G
 #define ALTERN_L3 KC_X, KC_J, KC_M, KC_C, KC_V
 
-#define ALTERN_R1 KC_QUOT, KC_Y, KC_O, KC_U, KC_Q
+#define ALTERN_R1 KC_QUOT, KC_Y, KC_O, KC_U, KC_MINS
 #define ALTERN_R2 KC_P, KC_H, KC_A, KC_E, KC_I
-#define ALTERN_R3 KC_K, KC_F, KC_DOT, KC_MINS, KC_COMM
+#define ALTERN_R3 KC_K, KC_F, KC_DOT, KC_Z, KC_COMM
 
 #endif
 
