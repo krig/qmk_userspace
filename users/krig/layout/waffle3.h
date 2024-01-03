421d9769805defcf730305a9535399f4957e9d46
Kristoffer Grönlund <k@ziran.se>
2024-01-03 15:21:24 +0100

    more waffles

--- users/krig/layout/waffle3.h
+++ users/krig/layout/waffle3.h
@@ -2,9 +2,9 @@
 // waffle3
 //
 // experimental layout
-/*   x c m g z  q y o u ' */
-/*   r s n t b  p h e i a */
-/*   w f l d v  j k - . , */
+/*   x c m g z  q y o u , */
+/*   r s n t b  k h e i a */
+/*   w f l d v  j p - . ' */
 #ifndef ALTERN_L1
 
 #define ALTERN_L1 KC_X, KC_C, KC_M, KC_G, KC_Z
@@ -12,8 +12,8 @@
 #define ALTERN_L3 KC_W, KC_F, KC_L, KC_D, KC_V
 
 #define ALTERN_R1 KC_Q, KC_Y, KC_O, KC_U, KC_COMM
-#define ALTERN_R2 KC_P, KC_H, KC_E, KC_I, KC_A
-#define ALTERN_R3 KC_J, KC_K, KC_MINS, KC_DOT, KC_QUOT
+#define ALTERN_R2 KC_K, KC_H, KC_E, KC_I, KC_A
+#define ALTERN_R3 KC_J, KC_P, KC_MINS, KC_DOT, KC_QUOT
 
 #endif
 
