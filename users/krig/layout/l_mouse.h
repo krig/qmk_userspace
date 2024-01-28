852ebf23e540a26552250788bbafbb42cb30de9e
Kristoffer Grönlund <k@ziran.se>
2024-01-28 22:08:55 +0100

    mpc4k had issues with w to s that I didn't like so going back to 2k

--- users/krig/layout/l_mouse.h
+++ users/krig/layout/l_mouse.h
@@ -4,7 +4,7 @@
 #define L_MOUSE_L2 KC_LALT, KC_LCTL, KC_LGUI, KC_LSFT, KC_RALT
 #define L_MOUSE_L3 _______, _______, _______, _______, _______
 
-#define L_MOUSE_R2 KC_ACL1, KC_MS_L, KC_MS_D, KC_MS_R, _______
 #define L_MOUSE_R1 KC_ACL0, KC_BTN1, KC_MS_U, KC_BTN2, KC_BTN3
+#define L_MOUSE_R2 KC_ACL1, KC_MS_L, KC_MS_D, KC_MS_R, _______
 #define L_MOUSE_R3 KC_ACL2, KC_WH_D, KC_WH_U, _______, _______
 
