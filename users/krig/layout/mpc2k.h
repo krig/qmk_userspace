6750551d06a8ce9a6574e1e6aa4f7a8e684f7cbf
Kristoffer Grönlund <k@ziran.se>
2024-02-06 09:53:21 +0100

    Revision of mpc - needs a new name now...

--- users/krig/layout/mpc2k.h
+++ users/krig/layout/mpc2k.h
@@ -1,18 +1,18 @@
 #pragma once
 // mpc2k
 //
-// q f h k j z w o u ,
-// r s n t m p c a e i
-// x b l d v g y ' / .
+// q w h f k j p o u ,
+// r s n t m y c a e i
+// x b l d v z g ' / .
 
 #ifndef ALTERN_L1
 
-#define ALTERN_L1 KC_Q, KC_F, KC_H, KC_K, KC_J
+#define ALTERN_L1 KC_Q, KC_W, KC_H, KC_F, KC_K
 #define ALTERN_L2 KC_R, KC_S, KC_N, KC_T, KC_M
 #define ALTERN_L3 KC_X, KC_B, KC_L, KC_D, KC_V
-#define ALTERN_R1 KC_Z, KC_W, KC_O, KC_U, KC_COMM
-#define ALTERN_R2 KC_P, KC_C, KC_A, KC_E, KC_I
-#define ALTERN_R3 KC_G, KC_Y, KC_QUOT, KC_SLSH, KC_DOT
+#define ALTERN_R1 KC_J, KC_P, KC_O, KC_U, KC_COMM
+#define ALTERN_R2 KC_Y, KC_C, KC_A, KC_E, KC_I
+#define ALTERN_R3 KC_Z, KC_G, KC_QUOT, KC_SLSH, KC_DOT
 
 #endif
 
