b536b7396ca1f4a39f7be6ae086685500173206e
Kristoffer Grönlund <k@ziran.se>
2024-02-14 21:00:31 +0100

    Update mpc2k.h

--- users/krig/layout/mpc2k.h
+++ users/krig/layout/mpc2k.h
@@ -3,7 +3,7 @@
 //
 // q w h f k j p o u ,
 // r s n t m y c a e i
-// x b l d v z g ' / .
+// x b l d v z g . ' /
 
 #ifndef ALTERN_L1
 
@@ -12,7 +12,7 @@
 #define ALTERN_L3 KC_X, KC_B, KC_L, KC_D, KC_V
 #define ALTERN_R1 KC_J, KC_P, KC_O, KC_U, KC_COMM
 #define ALTERN_R2 KC_Y, KC_C, KC_A, KC_E, KC_I
-#define ALTERN_R3 KC_Z, KC_G, KC_QUOT, KC_SLSH, KC_DOT
+#define ALTERN_R3 KC_Z, KC_G, KC_DOT, KC_QUOT, KC_SLSH
 
 #endif
 
