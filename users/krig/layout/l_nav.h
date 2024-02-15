318f1c1a5ffb848f563af1f0530f138e33548235
Kristoffer Grönlund <k@ziran.se>
2024-02-15 10:51:52 +0100

    rearrange nav layer a bit

--- users/krig/layout/l_nav.h
+++ users/krig/layout/l_nav.h
@@ -1,8 +1,8 @@
 // nav layer
 
-#define L_NAV_L1   LLOCK, GUI_SPC, CMD_GRV, CMD_TAB,  LCTL_W
+#define L_NAV_L1 CMD_TAB, CMD_GRV, GUI_SPC,  LCTL_W, MO(_NUM)
 #define L_NAV_L2 KC_LALT, KC_LCTL, KC_LGUI, KC_LSFT, KC_RALT
-#define L_NAV_L3  GUI__Z,  GUI__X,  GUI__C,  GUI__V, MO(_NUM)
+#define L_NAV_L3  GUI__Z,  GUI__X,  GUI__C,  GUI__V, LLOCK
 
 #define L_NAV_R1  KC_INS,  KC_DEL, _______, _______,  SWE_AA
 #define L_NAV_R2 KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,  SWE_AE
