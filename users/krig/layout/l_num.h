fd521e54b5a2865c276fce7a6b2b91005b84ec79
Kristoffer Grönlund <k@ziran.se>
2024-02-26 13:45:26 +0100

    micro-optimization for numpad - bad idea maybe

--- users/krig/layout/l_num.h
+++ users/krig/layout/l_num.h
@@ -6,8 +6,8 @@
 #define L_NUM_L2 KC_LALT, KC_LCTL, KC_LGUI, KC_LSFT, KC_RALT
 #define L_NUM_L3 _______, _______, _______, _______, _______
 
-#define L_NUM_R1 _______,    KC_1,    KC_2,    KC_3, _______
-#define L_NUM_R2 _______,    KC_4,    KC_5,    KC_6,    KC_0
+#define L_NUM_R1 _______,    KC_4,    KC_5,    KC_6, _______
+#define L_NUM_R2 _______,    KC_1,    KC_2,    KC_3,    KC_0
 #define L_NUM_R3 _______,    KC_7,    KC_8,    KC_9, _______
 
 #else
