0f31bc16af04733bd0a682e31d507d079e9f9932
Kristoffer Grönlund <k@ziran.se>
2024-02-27 10:07:56 +0100

    move swedish characters to better positions

--- users/krig/layout/l_nav.h
+++ users/krig/layout/l_nav.h
@@ -4,7 +4,7 @@
 #define L_NAV_L2 KC_LALT, KC_LCTL, KC_LGUI, KC_LSFT, MO(_NUM)
 #define L_NAV_L3  GUI__Z,  GUI__X,  GUI__C,  GUI__V, LLOCK
 
-#define L_NAV_R1  KC_INS,  KC_DEL, _______, _______,  SWE_AA
+#define L_NAV_R1  KC_INS,  KC_DEL,  SWE_AA,  SWE_OE, _______
 #define L_NAV_R2 KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,  SWE_AE
-#define L_NAV_R3 KC_HOME, KC_PGDN, KC_PGUP,  KC_END,  SWE_OE
+#define L_NAV_R3 KC_HOME, KC_PGDN, KC_PGUP,  KC_END, _______
 
