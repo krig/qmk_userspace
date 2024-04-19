267b1433e5b00e35a8e50b5a4b913f4873edf763
Kristoffer Grönlund <k@ziran.se>
2024-04-19 06:56:23 +0200

    more mouse key simplification and use of MK_COMBINED

--- users/krig/layout/l_mouse.h
+++ users/krig/layout/l_mouse.h
@@ -2,7 +2,7 @@
 
 #define L_MOUSE_L1 _______, _______, _______, _______, _______
 #define L_MOUSE_L2 KC_LALT, KC_LCTL, KC_LGUI, KC_LSFT, KC_RALT
-#define L_MOUSE_L3 _______, _______, _______, _______, _______
+#define L_MOUSE_L3 KC_ACL1, _______, _______, _______, _______
 
 #define L_MOUSE_R1 _______, KC_WH_D, KC_WH_U, _______, _______
 #define L_MOUSE_R2 KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______
