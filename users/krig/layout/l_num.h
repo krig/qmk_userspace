f51b850df6edaab21c3c632a94922e7c0abccb85
Kristoffer Grönlund <k@ziran.se>
2024-06-12 16:19:36 +0200

    Various symbol and numpad changes plus a screenshot key

--- users/krig/layout/l_num.h
+++ users/krig/layout/l_num.h
@@ -3,6 +3,6 @@
 #define L_NUM_L2 _______, _______, _______, _______, _______
 #define L_NUM_L3 _______, _______, _______, _______, _______
 
-#define L_NUM_R1 _______,    KC_4,    KC_5,    KC_6, _______
-#define L_NUM_R2 _______,    KC_1,    KC_2,    KC_3,    KC_0
-#define L_NUM_R3 _______,    KC_7,    KC_8,    KC_9, _______
+#define L_NUM_R1 KC_UNDS,    KC_1,    KC_2,    KC_3, KC_COMM
+#define L_NUM_R2 KC_COLN,    KC_4,    KC_5,    KC_6,    KC_0
+#define L_NUM_R3 KC_SLSH,    KC_7,    KC_8,    KC_9,  KC_DOT
