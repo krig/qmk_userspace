c8721ebcb3558497480b56ded7563f67460769a2
Kristoffer Grönlund <k@ziran.se>
2024-06-19 13:15:22 +0200

    make number keys on adjust do tab switching

--- users/krig/layout/adj34.h
+++ users/krig/layout/adj34.h
@@ -1,8 +1,8 @@
-#define L_ADJ34_L1 KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5
-#define L_ADJ34_L2 KC_1,    KC_2,    KC_3,    KC_4,    KC_5
+#define L_ADJ34_L1   KC_F1,   KC_F2,     KC_F3,     KC_F4,     KC_F5
+#define L_ADJ34_L2 G(KC_1), G(KC_2),   G(KC_3),   G(KC_4),   G(KC_5)
 #define L_ADJ34_L3 DF_QWER, DF_GAME, LSG(KC_3), LSG(KC_4), LSG(KC_5)
 
 #define L_ADJ34_R1 KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10
-#define L_ADJ34_R2 KC_6,    KC_7,    KC_8,    KC_9,    KC_0
+#define L_ADJ34_R2 SGUI(KC_1), SGUI(KC_2), SGUI(KC_3), SGUI(KC_4), SGUI(KC_5)
 #define L_ADJ34_R3 _______, _______, _______, _______, QK_BOOT
 
