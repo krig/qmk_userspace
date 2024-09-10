e19f0b9f26440170c52f28afcc5b384b5d2130d7
Kristoffer Grönlund <k@ziran.se>
2024-09-10 20:45:51 +0200

    use extra thumb keys to map all alphas on gaming layer

--- keyboards/beekeeb/3w6hs/keymaps/krig/keymap.c
+++ keyboards/beekeeb/3w6hs/keymaps/krig/keymap.c
@@ -24,10 +24,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     THUMB_L3, THUMB_L2, THUMB_L1, THUMB_R1, THUMB_R2, THUMB_R3
   ),
   [_GAME] = LAYOUT_wrapper(
-     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-    KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,     KC_H,    KC_J,    KC_K,    KC_L, KC_QUOT,
-    KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,     KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
-    THUMB_L3, THUMB_L2, THUMB_L1, THUMB_R1, THUMB_R2, THUMB_R3
+     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,
+    KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,     KC_G,    KC_H,    KC_J,    KC_K,    KC_L,
+    KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT,
+    KC_SLSH, THUMB_L2, THUMB_L1, THUMB_R1, THUMB_R2, KC_P
   ),
   [_NAV] = LAYOUT_wrapper(
     L_NAV_L1, L_NAV_R1,
