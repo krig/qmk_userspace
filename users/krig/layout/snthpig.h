6919aab8917fe0a4a266f23fe3c7072aa4d2ab5a
Kristoffer Grönlund <k@ziran.se>
2025-01-31 16:47:18 +0100

    Thinking about HRM

--- users/krig/layout/snthpig.h
+++ users/krig/layout/snthpig.h
@@ -3,9 +3,20 @@
 // q f h k v j y o u ö
 // r s n t p g c a e i
 // x m l d b z w . å ä
+
 #define QWERTY_L1 KC_Q, KC_F, KC_H, KC_K, KC_V
 #define QWERTY_L2 KC_R, KC_S, KC_N, KC_T, KC_P
-#define QWERTY_L3 KC_X, KC_M, KC_L, LT(_NUM, KC_D), KC_B
 #define QWERTY_R1 KC_J, KC_Y, KC_O, KC_U, SWE_OE
 #define QWERTY_R2 KC_G, KC_C, KC_A, KC_E, KC_I
+
+#ifdef KRIG_HOMEROWMODS
+
+#define QWERTY_L3 ALT_T(KC_X), CTL_T(KC_M), CMD_T(KC_L), LT(_NUM, KC_D), KC_B
+#define QWERTY_R3 KC_Z, KC_W, CMD_T(KC_DOT), CTL_T(SWE_AA), ALT_T(SWE_AE)
+
+#else
+
+#define QWERTY_L3 KC_X, KC_M, KC_L, LT(_NUM, KC_D), KC_B
 #define QWERTY_R3 KC_Z, KC_W, KC_DOT, SWE_AA, SWE_AE
+
+#endif
